d472188d5e603bfd36484477e4a6c2bdea781f6f
Raz Damaschin <razdjp@gmail.com>
2017-12-23 11:32:33 -0600

    Added pagination

--- app/forms.py
+++ app/forms.py
@@ -44,3 +44,8 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username already exists.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[DataRequired()])
+    submit = SubmitField('Submit')
