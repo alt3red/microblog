b50f58321f39c2f4ce59cb616e687f8e62f9a5e3
Raz Damaschin <razdjp@gmail.com>
2017-12-30 11:03:31 -0600

    Added Blueprints and .env file

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to Microblog
+
+This is an example application using Flask, written by Miguel Grinberg.
+
+### Config
+
+You will need a .env file with the following:
+
+ex:
+
+SECRET_KEY=a-really-long-and-unique-key-that-nobody-knows
+MAIL_SERVER=localhost
+MAIL_PORT=25
+MS_TRANSLATOR_KEY=<your-translator-key-here>
--- tests.py
+++ tests.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
@@ -21,8 +31,9 @@ class UserModelCase(unittest.TestCase):
 
     def test_avatar(self):
         u = User(username='john', email='john@example.com')
-        self.assertEqual(u.avatar(128),
-                         'https://www.gravatar.com/avatar/d4c74594d841139328695756648b6bd6?d=identicon&s=128')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
@@ -47,16 +58,6 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
 
-
-class TestCase(unittest.TestCase):
-    def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-        db.create_all()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
     def test_follow_posts(self):
         # create four users
         u1 = User(username='john', email='john@example.com')
@@ -79,13 +80,13 @@ class TestCase(unittest.TestCase):
         db.session.commit()
 
         # setup the followers
-        u1.follow(u2)   # john follows susan
-        u1.follow(u4)   # john follows david
-        u2.follow(u3)   # susan follows mary
-        u3.follow(u4)   # mary follows davis
+        u1.follow(u2)  # john follows susan
+        u1.follow(u4)  # john follows david
+        u2.follow(u3)  # susan follows mary
+        u3.follow(u4)  # mary follows david
         db.session.commit()
 
-        # check the followed posts of each other
+        # check the followed posts of each user
         f1 = u1.followed_posts().all()
         f2 = u2.followed_posts().all()
         f3 = u3.followed_posts().all()
